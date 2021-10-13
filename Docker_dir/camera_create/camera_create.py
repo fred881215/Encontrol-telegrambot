362bb88e6c84c3388a7e97c70a6d4e98966260f8
fred881215 <59314905+fred881215@users.noreply.github.com>
2021-10-13 13:04:42 +0800

    Update camera_create.py

--- Docker_dir/camera_create/camera_create.py
+++ Docker_dir/camera_create/camera_create.py
@@ -1,9 +1,11 @@
+from kubernetes import client, config as kubeconfig, utils
 import telegram
 from pymongo import MongoClient
 import configparser
 import time
 import schedule
 import socket
+import os
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -20,6 +22,10 @@ dbCameraControl = myMongoDb['cameraControl']
 dbTelegramBot_Token = myMongoDb['TelegramBot_Token']
 TGToken = dbTelegramBot_Token.find_one()
 bot = telegram.Bot(token=(TGToken["token"]))
+
+# 連通K8s client
+kubeconfig.load_incluster_config()
+k8s_client = client.ApiClient()
 
 # 攝像機位址連接測試
 def socket_connection(device_ip, device_port):
@@ -46,14 +52,104 @@ def main():
             if result == 0: 
                 respText = "攝像機新增完成～"
                 # 新增攝像機資料, 編號自動遞增
-                count_list = len([i for i in dbCameraControl.find()])
-                dbCameraControl.update_one({"device_number":str(count_list+1)}, {"$set":{"status":"0", "device_name":request["name"], "device_location":request["location"], "device_ip":request["ip"], "device_port":request["port"], "account":request["account"], "pin_code":request["pin_code"], "url":request["url"], "fps":request["fps"], "nfs_dir":request["nfs_dir"], "video_second":"", "chat_id":"", "connection":"0"}}, upsert=True)
+                # count_list = len([i for i in dbCameraControl.find()])
+                # dbCameraControl.update_one({"device_number":str(count_list+1)}, {"$set":{"status":"0", "device_name":request["name"], "device_location":request["location"], "device_ip":request["ip"], "device_port":request["port"], "account":request["account"], "pin_code":request["pin_code"], "url":request["url"], "fps":request["fps"], "storage_space":request["storage_space"], "nfs_dir":request["nfs_dir"], "video_second":"", "chat_id":"", "connection":"0"}}, upsert=True)
+                # 設定串流Yaml檔名
+                filename = "fetchcron.yaml"
+                # 判斷Rtsp Url格式
+                if request['account'] == "null" and request['pin_code'] == "null":
+                    yaml_url = f"rtsp://{request['ip']}:{request['port']}{request['url']}"
+                else:
+                    yaml_url = f"rtsp://{request['account']}:{request['pin_code']}@{request['ip']}:{request['port']}{request['url']}"
+                # nfs_dir 大寫轉小寫, Yaml 檔 metadata_name 欄位內容只可以包含小寫英文、數字、符號[-.]
+                metadata_name = request["nfs_dir"].lower()
+                # 讀取串流Yaml, 依照攝像機資料修改欄位內容
+                with open(filename, "w+", encoding='utf8') as yaml:
+                    yaml_data = f'apiVersion: batch/v1beta1\n\
+kind: CronJob\n\
+metadata:\n\
+  name: cron-camera-{metadata_name}\n\
+  namespace: default\n\
+spec:\n\
+  schedule: "*/5 * * * *"\n\
+  jobTemplate:\n\
+    spec:\n\
+      template:\n\
+        spec:\n\
+          containers:\n\
+          - name: thisiscontainername\n\
+            image: vincent5753/capture\n\
+            volumeMounts:\n\
+            - name: nfs-volume\n\
+              mountPath: //mnt/telegram\n\
+            env:\n\
+            - name: STOREPATH\n\
+              value: "/mnt/telegram"\n\
+            - name: NAME\n\
+              value: "{request["nfs_dir"]}"\n\
+            - name: URL\n\
+              value: "{yaml_url}"\n\
+            command: ["/bin/bash","-c"]\n\
+            args: ["bash /var/capture/capture.sh $STOREPATH $NAME $URL"]\n\
+          restartPolicy: OnFailure\n\
+          volumes:\n\
+          - name: nfs-volume\n\
+            nfs:\n\
+              server: 10.0.0.173\n\
+              path: /var/nfsshare/volume1/surveillance'
+                    yaml.write(yaml_data)
+                    yaml.close()
+                # 設定監控Yaml檔名
+                filename = "deletecron.yaml"
+                # 讀取監控Yaml, 依照攝像機資料修改欄位內容
+                with open(filename, "w+", encoding='utf8') as yaml:
+                    yaml_data = f'apiVersion: batch/v1beta1\n\
+kind: CronJob\n\
+metadata:\n\
+  name: cron-delete-{metadata_name}\n\
+  namespace: default\n\
+spec:\n\
+  schedule: "*/30 * * * *"\n\
+  jobTemplate:\n\
+    spec:\n\
+      template:\n\
+        spec:\n\
+          containers:\n\
+          - name: thisiscontainername\n\
+            image: vincent5753/delete\n\
+            volumeMounts:\n\
+            - name: nfs-volume\n\
+              mountPath: //mnt/telegram\n\
+            env:\n\
+            - name: STOREPATH\n\
+              value: "/mnt/telegram"\n\
+            - name: NAME\n\
+              value: "{request["nfs_dir"]}"\n\
+            - name: KSI\n\
+              value: {request["storage_space"]}\n\
+            command: ["/bin/bash","-c"]\n\
+            args: ["bash /var/capture/delete.sh $STOREPATH $NAME $KSI"]\n\
+          restartPolicy: OnFailure\n\
+          volumes:\n\
+          - name: nfs-volume\n\
+            nfs:\n\
+              server: 10.0.0.173\n\
+              path: /var/nfsshare/volume1/surveillance'
+                    yaml.write(yaml_data)
+                    yaml.close()
+                # 部署修改後的兩個Yaml檔
+                filename = "fetchcron.yaml"
+                os.system("kubectl apply -f " + filename)
+                # utils.create_from_yaml(k8s_client, filename)
+                filename = "deletecron.yaml"
+                os.system("kubectl apply -f " + filename)
+                # utils.create_from_yaml(k8s_client, filename)
             else:
                 respText = "該位址無法連通, 請修正後重新嘗試～"
             # 測試結果回傳給使用者
             bot.send_message(chat_id=request["chat_id"], text=respText, parse_mode="Markdown")
             # 使用者請求清空
-            dbCameraCreate.update_one({"feature":"datapost"}, {"$set":{"status":"0", "name":"", "location":"", "ip":"", "port":"", "account":"", "pin_code":"", "url":"", "chat_id":""}}, upsert=True)
+            dbCameraCreate.update_one({"feature":"datapost"}, {"$set":{"status":"0", "name":"", "location":"", "ip":"", "port":"", "account":"", "pin_code":"", "url":"", "fps":"", "storage_space":"", "nfs_dir":"", "chat_id":""}}, upsert=True)
     func_CameraCreate()
 main()
 
